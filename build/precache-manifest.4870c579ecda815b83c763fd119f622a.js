4d9d6dc0f6f2f03b5bc81acbc84d2fc086647067
fyodore82 <fyodor_e@hotmail.com>
2019-12-07 13:59:39 +1000

    a

---
+++ build/precache-manifest.4870c579ecda815b83c763fd119f622a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "793d715fcd36667fa39268192966cdd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "489f27dc1034e1e8f13c",
+    "url": "/static/js/2.a9f92573.chunk.js"
+  },
+  {
+    "revision": "9f8b15cf42f96bb775b2",
+    "url": "/static/js/main.1f0d4fd6.chunk.js"
+  },
+  {
+    "revision": "ffa8e9db3ece4d19e766",
+    "url": "/static/js/runtime-main.4e3a3e07.js"
+  }
+]);
