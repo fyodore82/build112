8fa5b71dc1de30ab6d17cea992d3adc1e635c20d
fyodore82 <fyodor_e@hotmail.com>
2020-03-27 08:51:19 +1000

    Inv build

---
+++ inv/precache-manifest.0ee9731b55514b178b9d8da3a7c393ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e30e3d1157979d984174a059c3a89e98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3764fba97f2dcfe127c9",
+    "url": "/static/css/main.ac4ca1a2.chunk.css"
+  },
+  {
+    "revision": "d47c08df85bd151ac694",
+    "url": "/static/js/2.6082df2e.chunk.js"
+  },
+  {
+    "revision": "45e78cf8d545cb6a9d6682654ac770f1",
+    "url": "/static/js/2.6082df2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3764fba97f2dcfe127c9",
+    "url": "/static/js/main.8fb85698.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
